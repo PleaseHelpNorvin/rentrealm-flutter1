8e61f7467b6122ef13d3b8c215b435be6b202f3d
PleaseHelpNorvin <140196018+PleaseHelpNorvin@users.noreply.github.com>
2025-02-17 14:00:11 +0800

    clean main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rentealm_flutter/PROVIDERS/room_provider.dart';
-// import 'package:rentealm_flutter/PROVIDERS/tenant_provider.dart';
-// import 'package:rentrealm_flutter2/PROVIDERS/profile_provider.dart';
-import './PROVIDERS/profile_provider.dart';
-// import 'package:rentrealm_flutter2/PROVIDERS/user_provider.dart';
-import './PROVIDERS/user_provider.dart';
-
-// import 'package:rentrealm_flutter2/SCREENS/AUTH/login.dart';
-import './SCREENS/AUTH/login.dart';
-// import 'package:rentrealm_flutter2/SCREENS/PROFILE/UPDATE/edit_address_screen.dart';
-import './SCREENS/PROFILE/UPDATE/edit_address_screen.dart';
-import './SCREENS/PROFILE/UPDATE/edit_identification_screen.dart';
-
-import './SCREENS/PROFILE/UPDATE/edit_profile_screen.dart';
-
-import './SCREENS/PROFILE/UPDATE/edit_user_screen.dart';
-
-// import 'package:rentrealm_flutter2/SCREENS/PROFILE/UPDATE/edit_identification_screen.dart';
-// import 'package:rentrealm_flutter2/SCREENS/PROFILE/UPDATE/edit_profile_screen.dart';
-// import 'package:rentrealm_flutter2/SCREENS/PROFILE/UPDATE/edit_user_screen.dart';
 
 import 'PROVIDERS/auth_provider.dart';
 import 'PROVIDERS/theme_provider.dart';
 import 'PROVIDERS/tenant_provider.dart';
 import 'PROVIDERS/property_provider.dart';
+import './PROVIDERS/profile_provider.dart';
+import './PROVIDERS/user_provider.dart';
 
-import 'SCREENS/AUTH/register.dart';
+import './SCREENS/AUTH/login.dart';
+import './SCREENS/AUTH/register.dart';
+import './SCREENS/PROFILE/UPDATE/edit_address_screen.dart';
+import './SCREENS/PROFILE/UPDATE/edit_identification_screen.dart';
+import './SCREENS/PROFILE/UPDATE/edit_profile_screen.dart';
+import './SCREENS/PROFILE/UPDATE/edit_user_screen.dart';
 import 'SCREENS/PROFILE/CREATE/create_profile_screen1.dart';
 import 'SCREENS/get_started.dart';
 import 'SCREENS/TENANT/CREATE/create_tenant_screen1.dart';
